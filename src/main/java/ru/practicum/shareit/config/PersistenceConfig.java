2c9c348c601ed9a7cf661123e290e102de5f3d61
Rustem <testunix2014@yandex.ru>
2025-07-20 15:01:46 +0500

    Исправления ошибок, не проходят тесты github

--- src/main/java/ru/practicum/shareit/config/PersistenceConfig.java
+++ src/main/java/ru/practicum/shareit/config/PersistenceConfig.java
@@ -1,18 +1,5 @@
 package ru.practicum.shareit.config;
 
-import jakarta.persistence.EntityManagerFactory;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 //@EnableJpaRepositories(basePackages = "ru.practicum.shareit")
 //@Configuration

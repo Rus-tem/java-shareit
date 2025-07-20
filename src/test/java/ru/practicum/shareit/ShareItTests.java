b4e68020fbbbb86ce67f904aa41eede12010ef73
Rustem <testunix2014@yandex.ru>
2025-07-20 13:23:04 +0500

    Исправления ошибок, не проходят тесты github

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -5,11 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest
 class ShareItTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
